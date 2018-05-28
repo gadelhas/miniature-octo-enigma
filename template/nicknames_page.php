a0c3eed198385624c11ee364f5b6b7d480e1ca73
Mário Santos <marionbsantos@sapo.pt>
2018-05-28 14:11:45 +0100

    add l18n to template

--- template/nicknames_page.php
+++ template/nicknames_page.php
@@ -4,23 +4,23 @@
  * @var string $nicknames
  */
 ?>
-<h3>Information about nicknames</h3>
+<h3><?php _e( "Information about nicknames", "saucal" ); ?></h3>
 <?php
 foreach ( $nickname_results as $key => $result ): ?>
     <p><strong><?php echo $key; ?>:</strong> <?php echo $result; ?></p>
 <?php endforeach; ?>
 <hr>
-<h3>Add your nicknames here</h3>
+<h3><?php _e( "Add your nicknames here", "saucal" ); ?></h3>
 
 <form action="" method="POST">
     <div class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-        <label for="nicknames_list">Please enter here your nicknames: (One per line)</label>
+        <label for="nicknames_list"><?php _e( "Please enter here your nicknames: (One per line)", "saucal" ); ?></label>
         <textarea name="nicknames_list" id="nicknames_list" rows="5"><?php echo $nicknames; ?></textarea>
     </div>
     <div class="clear"></div>
     <br>
     <p>
 		<?php wp_nonce_field( "saucal_retriever", "_wpnonce" ); ?>
-        <button type="submit" class="button">Update</button>
+        <button type="submit" class="button"><?php _e( "Update", "saucal" ); ?></button>
     </p>
 </form>
